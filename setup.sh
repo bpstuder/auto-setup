a0d3140b27899dc7335262c994fb988bcfe450af
Benoit-Pierre STUDER <37024586+bpstuder@users.noreply.github.com>
2021-07-08 21:38:02 +0200

    Updated Logging function

--- setup.sh
+++ setup.sh
@@ -44,54 +44,95 @@ APPLICATION_LIST=(
     "zoom"
 )
 
+INSTALL_OH_MY_ZSH='true'
+OH_MY_ZSH_THEME='amuse'
+LOG="./auto-setup.log"
+
 #####################
 # Main code
 #####################
 
+function LogVerbose () {
+    case ${1} in
+    ok)
+        echo -e "\U2705 - ${2}" | tee -a "${LOG}"
+        ;;
+    warning)
+        echo -e "\U26A0 - ${2}" | tee -a "${LOG}"
+        ;;
+    error)
+        echo -e "\U274C - ${2}" | tee -a "${LOG}"
+        ;;
+    info)
+        echo -e "\U2139 - ${2}" | tee -a "${LOG}"
+        ;;
+    *)
+        echo -e "\U2753 - ${2}" | tee -a "${LOG}"
+        ;;
+    esac
+}
+
 # Ensure Apple's command line tools are installed
 if ! which cc >/dev/null; then
-    echo "Installing Xcode Command Line Tools"
+    LogVerbose "info" "Installing Xcode Command Line Tools"
     xcode-select --install
-    echo "\U2705 - Done"
+    LogVerbose "ok" "Done"
 else
-    echo "\U2705 - Xcode already installed. Skipping."
+    LogVerbose "ok" "Xcode already installed. Skipping."
 fi
 
 if ! which brew >/dev/null; then
-    echo "Installing Homebrew..."
+    LogVerbose "info" "Installing Homebrew..."
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-    echo "Registering Homebrew"
+    LogVerbose "info" "Registering Homebrew"
     echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >>~/.zprofile
     eval "$(/opt/homebrew/bin/brew shellenv)"
-    echo "\U2705 - Done"
+    LogVerbose "ok" "Done"
 else
-    echo "\U2705 - Homebrew already installed. Skipping."
+    LogVerbose "ok" "Homebrew already installed. Skipping."
 fi
 
 for TAP in "${TAP_LIST[@]}"; do
-    echo "Processing tap : ${TAP}"
+    LogVerbose "info" "Processing tap : ${TAP}"
     brew tap ${TAP}
-    echo "\U2705 - Done"
+    LogVerbose "ok" "Done"
 done
 
 for APPLICATION in "${APPLICATION_LIST[@]}"; do
-    echo "Processing: ${APPLICATION}"
-    echo "Checking if ${APPLICATION} is installed"
+    # LogVerbose "info" "Processing: ${APPLICATION}"
+    LogVerbose "info" "Checking if ${APPLICATION} is installed"
     IS_INSTALLED=$(brew list -1 | grep ${APPLICATION})
     if [[ -z ${IS_INSTALLED} ]]; then
         brew install ${APPLICATION} --force >> /dev/null
-        echo -e "\U2705 - Done"
+        LogVerbose "ok" "Done"
     else
-        echo -e "\U2705 - ${APPLICATION} already installed"
+        LogVerbose "ok" "${APPLICATION} already installed"
     fi
 done
 
-echo "Checking if Brew Autoupdate is set up"
+LogVerbose "info" "Checking if Brew Autoupdate is set up"
 AUTOUPDATE_STATUS=$(brew autoupdate status | awk -F '.' '{print $1}')
 if [[ "${AUTOUPDATE_STATUS}" == "Autoupdate is not configured" ]]; then
-    echo "Setting Autoupdate"
+    LogVerbose "info" "Setting Autoupdate"
     brew autoupdate start --upgrade --cleanup --enable-notification
-    echo -e "\U2705 - Done"
+    LogVerbose "ok" "Done"
 else
-    echo -e "\U2705 - Autoupdate running"
+    LogVerbose "ok" "Autoupdate running"
 fi
+
+if [[ ${INSTALL_OH_MY_ZSH} == true ]]; then
+    LogVerbose "info" "Installing Oh My Zsh"
+    curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
+    if [[ $? == 0 ]]; then
+        LogVerbose "ok" "Done"
+        LogVerbose "info" "Changing theme to ${OH_MY_ZSH_THEME}"
+        sed -i '' "s/robbyrussell/${OH_MY_ZSH_THEME}/g" .zshrc
+        LogVerbose "ok" "Done"
+    else
+        LogVerbose "error" "Unable to install Oh My Zsh"
+    fi
+else
+    LogVerbose "info" "Skipping Oh My Zsh setup"
+fi
+
+LogVerbose "ok" "Script execution complete"
