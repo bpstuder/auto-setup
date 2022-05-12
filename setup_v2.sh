9848571cfeed3071b906e407092ba07bf4963db0
Benoit-Pierre STUDER <37024586+bpstuder@users.noreply.github.com>
2022-05-12 09:07:50 +0200

    Create setup_v2.sh

---
+++ setup_v2.sh
@@ -0,0 +1,54 @@
+#!/bin/zsh
+###
+# File: setup_v2.sh
+# File Created: 2022-02-20 11:57:01
+# Usage :
+# Author: Benoit-Pierre STUDER
+# -----
+# HISTORY:
+###
+
+appsInstallomator=(
+    "appcleaner"
+    "balenaetcher"
+    "discord"
+    "dropbox"
+    "firefox"
+    "golang"
+    "githubdesktop"
+    "handbrake"
+    "iina"
+    "istatmenus"
+    "iterm2"
+    "keepassxc"
+    "logitechoptions"
+    "macfuse"
+    "microsoftoffice365"
+    "postman"
+    "rectangle"
+    "slack"
+    "suspicious-package"
+    "teamviewerhost"
+    "theunarchiver"
+    "veracrypt"
+    "visualstudiocode"
+    "whatsapp"
+    "zoom"
+)
+
+appsHomebrew=(
+    "svn" #Prerequisite for fonts
+    # "autopkgr"
+    "cyberduck"
+    # "disk-inventory-x"
+    "font-inconsolata"
+    "font-roboto"
+    "font-clear-sans"
+    "font-fira-code"
+    "font-source-code-pro"
+    "mactracker"
+    "plex"
+    "powershell"
+    "profilecreator"
+    "unnaturalscrollwheels"
+)
